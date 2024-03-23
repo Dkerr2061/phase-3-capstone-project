9539d0f40e9c294a9c97e80aa5881c54da277665
David Kerr <kerrky@gmail.com>
2024-03-22 23:55:19 -0400

    Moved the load bad code to load_bar.py

--- lib/cli.py
+++ lib/cli.py
@@ -1,30 +1,12 @@
 # lib/cli.py
 import colorama
-import math
+
+from load_bar import *
 from helpers import (
     exit_program,
     helper_1
 )
-# Loading Bar code
-colorama.init()
 
-def progress_bar(progress, total, color=colorama.Fore.YELLOW):
-    percent = 100 * (progress / float(total))
-    bar = '■' * int(percent) + '-' * (100 - int(percent))
-    print(color + f"\r[{bar}] {percent:.2f}%", end="\r")
-    if progress == total:
-        print(colorama.Fore.GREEN + f"\r[{bar}] {percent:.2f}%", end="\r")
-
-numbers = [x * 5 for x in range(2000, 3000)]
-results = []
-
-progress_bar(0, len(numbers))
-for i, x in enumerate(numbers):
-    results.append(math.factorial(x))
-    progress_bar(i + 1, len(numbers))
-
-print(colorama.Fore.RESET)
-###################################################################
 
 def main():
     while True:
@@ -39,11 +21,12 @@ def main():
 
 
 def menu():
-    print(colorama.Fore.RED + "Please select an option:")
+    print(colorama.Fore.LIGHTRED_EX + "Please select an option:")
     print(colorama.Fore.RESET)
-    print(colorama.Fore.BLUE + "0. Exit the program")
-    print(colorama.Fore.BLUE + "1. Some useful function")
+    print(colorama.Fore.LIGHTCYAN_EX + "1. Some useful function")
+    print(colorama.Fore.LIGHTCYAN_EX + "0. Exit the program")
     print(colorama.Fore.RESET)
+    
 
 
 
---
+++ lib/seed.py
@@ -0,0 +1,4 @@
+from models.artist import Artist
+from models.album import Album
+
+from models.__init__ import CURSOR, CONN
